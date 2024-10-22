27ce6bba086db19241da2807436bb4e33557c5b4
Muneer Abrahams <audipunkwp@gmail.com>
2024-10-22 16:21:45 +0200

    update

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -39,7 +39,7 @@ function findIntersection(setA, setB) {
 async function navigateLabyrinth(directions) {
     for (let direction of directions) {
         await new Promise((resolve) => setTimeout(resolve, 2000));
-        console.log(`Navigating: ${direction.step}`);
+        console.log(`Navigating': '${direction.step}`);
     };
     return "Congratulations! You've mastered the essentials of Vanilla JavaScript. Welcome to the world of React, where you'll build powerful and dynamic web applications. Let's dive in!";
 };
